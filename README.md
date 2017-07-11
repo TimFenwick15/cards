2502d130b7a9c50b94e3f20e0ed77a0b71be4062
Tim Fenwick <fent@ncode.com>
2017-07-11 13:15:57 +0100

    Top info bar can float or lock

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A start of a card based site.
 
 The data for the cards is fetched from the server. These cards are just text files for now.
+
+Issue: In single column, floating #info mode, #info is 10px too wide :(
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('working');
 
 const renderCard = function(text) {
   const card = document.getElementById('cards');
@@ -7,9 +6,28 @@ const renderCard = function(text) {
   div.innerHTML = text;
   card.appendChild(div);
 }
+
 let cardData = [];
 fetch('http://localhost:1337/card')
   .then(res => res.json())
   .then(res => res.forEach(renderCard));
-  //.then(res => res.forEach(alert));
+
+
+let lockToTop = false;
+const infoStyles = function() {
+
+  const info = document.getElementById('info');
+
+  if (lockToTop && document.body.scrollTop === 0) {
+    // Apply the floating style
+    info.setAttribute('style', 'width: calc(80% + 20px);height: 150px;left: 50%;transform: translate(-50%, 0);margin: 10 auto;');
+    lockToTop = false;
+  }
+  if (!lockToTop && document.body.scrollTop !== 0) {
+    // Apply the locked style
+    info.setAttribute('style', 'width: 100%;height: 50px;position: fixed;margin: 0;');
+    lockToTop = true;
+  }
+}
+setInterval(infoStyles, 100);
 
